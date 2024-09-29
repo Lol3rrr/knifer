a5716e11dc1ce1f98ef54b7beb63007db375d04a
Lol3rrr <lol3r@lol3r.net>
2024-09-29 13:15:00 +0200

    Add inferno test demo

--- analysis/tests/heatmap.rs
+++ analysis/tests/heatmap.rs
@@ -11,12 +11,22 @@ fn heatmap_nuke() {
     let config = heatmap::Config { cell_size: 5.0 };
     let (result, players) = heatmap::parse(&config, &input_bytes).unwrap();
 
-    for (user, mut heatmap) in result {
-        heatmap.shrink();
+    assert_eq!(result.len(), players.len());
 
-        let image = heatmap.as_image();
-        image.save_with_format(format!("./heatmap_nuke-{}.png", user.0), image::ImageFormat::Png).unwrap();
-    }
+    todo!()
+}
+
+#[test]
+#[traced_test]
+fn heatmap_inferno() {
+    let path = concat!(env!("CARGO_MANIFEST_DIR"), "/../testfiles/inferno.dem");
+    dbg!(path);
+    let input_bytes = std::fs::read(path).unwrap();
+
+    let config = heatmap::Config { cell_size: 5.0 };
+    let (result, players) = heatmap::parse(&config, &input_bytes).unwrap();
+
+    assert_eq!(result.len(), players.len());
 
     todo!()
 }
